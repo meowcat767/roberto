1e18a0e9e4508fed76e28f574c2693ca77e6e050
meowcat767 <meowcat@meowcat.site>
2026-03-06 14:49:42 +0000

    intial

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2441271.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 278961896, 278967573, 278967604, 278968346, 278968347 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/278967062/1/basis.wasm.js', 'wasmUrl' : 'files/assets/278967060/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/278967061/1/basis.js', 'preload' : false},
+];
